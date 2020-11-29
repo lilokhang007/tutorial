498ad997f979dcd774aa0ca4de75244b339272d0
lilokhang <lilokhang007@gmail.com>
2020-11-29 11:32:09 +0800

    encrypt

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 from jinja2.exceptions import TemplateSyntaxError
 from staticjinja import Site
 from distutils.dir_util import copy_tree
+import os
+from shutil import move
 
 if __name__ == "__main__":
 	copy_tree('static', 'docs')
@@ -12,3 +14,6 @@ if __name__ == "__main__":
 	except TemplateSyntaxError as error:
 		print('Template syntax error:', error.filename, 'line', error.lineno)
 		print(error.message)
+
+	os.system('staticrypt docs/tutorial1.html tko123;')
+	move('docs/tutorial1_encrypted.html','docs/tutorial1.html')
